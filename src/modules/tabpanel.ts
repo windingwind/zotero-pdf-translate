d43fe4654f33476dfb4aa41de6de51ed78c30282
windingwind <wyzlshx@foxmail.com>
2023-02-16 16:12:16 +0800

    fix: #368

--- src/modules/tabpanel.ts
+++ src/modules/tabpanel.ts
@@ -40,6 +40,7 @@ export function registerReaderTabPanel() {
     ?.addEventListener("click", (ev) => {
       updateTextAreasSize();
     });
+  updateTextAreasSize(true);
 }
 
 async function openWindowPanel() {
