439b21e26e7a602c2ca7d50fd11db133b0b38125
MuiseDestiny <51939531+MuiseDestiny@users.noreply.github.com>
2023-02-20 18:35:40 +0800

    add: two display mode && resizer

--- src/modules/prompt.ts
+++ src/modules/prompt.ts
@@ -3,44 +3,32 @@ import { config } from "../../package.json";
 export function registerPrompt() {
   ztoolkit.Prompt.register([{
     name: "Translate Sentences",
-    label: config.addonName,
+    label: config.addonInstance,
     when: () => {
       const selection = ztoolkit.Reader.getSelectedText(
         Zotero.Reader.getByTabID(Zotero_Tabs.selectedID)
       );
-      ztoolkit.log(selection)
-      return selection.length > 0
+      const sl = Zotero.Prefs.get("ZoteroPDFTranslate.sourceLanguage") as string
+      const tl = Zotero.Prefs.get("ZoteroPDFTranslate.targetLanguage") as string
+      return selection.length > 0 && Zotero?.PDFTranslate && sl.startsWith("en") && tl.startsWith("zh")
     },
-    callback: (prompt) => {
+    callback: async (prompt) => {
       const selection = ztoolkit.Reader.getSelectedText(
         Zotero.Reader.getByTabID(Zotero_Tabs.selectedID)
       );
-      const container = prompt.createCommandsContainer() as HTMLDivElement
-      container.style = `
-          padding: .5em;
-          display: flex;
-          flex-direction: row;
-          justify-content: space-between;
-        `
       const queue = Zotero.PDFTranslate.data.translate.queue
-      const task = queue.find((i: any) => i.raw == selection) || queue.slice(-1)[0]
+      let task = queue.find((task: any) => task.raw == selection && task.result.length > 0)
       if (!task) {
-        prompt.showTip("Task is Null.")
+        prompt.showTip("Loading...")
+        task = await Zotero.PDFTranslate.api.translate(selection)
+        Zotero.PDFTranslate.data.translate.queue.push(task)
+        // @ts-ignore
+        prompt.exit()
       }
+      console.log(task)
+      prompt.inputNode.placeholder = task.service
       const rawText = task.raw, resultText = task.result;
-      const props = {
-        styles: {
-          width: "49%",
-          height: "20em",
-          border: "1px solid #eee",
-          textAlign: "justify",
-          padding: ".5em",
-          fontSize: "1em",
-          lineHeight: "1.5em",
-          overflowY: "auto",
-        },
-      }
-      // TODO: prefs
+
       let addSentences = (node: HTMLElement, text: string, sep: string) => {
         let sentences = text.match(new RegExp(`.+?${sep}\\s*`, "g"))! as string[]
         let i = 0
@@ -82,26 +70,107 @@ export function registerPrompt() {
                   const parentNode = span.parentNode as HTMLDivElement
                   parentNode?.querySelectorAll("span").forEach(e => e.style.backgroundColor = "")
                   span.style.backgroundColor = hightlightColor
-                  const siblingNode = (parentNode?.previousSibling || parentNode?.nextSibling) as HTMLDivElement
+                  const siblingNode = (parentNode?.previousSibling?.previousSibling || parentNode?.nextSibling?.nextSibling) as HTMLDivElement
                   siblingNode?.querySelectorAll("span").forEach(e => e.style.backgroundColor = "");
                   const twinSpan = siblingNode.querySelector(`span[id=sentence-${i}]`) as HTMLSpanElement
                   twinSpan.style.backgroundColor = hightlightColor;
-                  siblingNode.scrollTo(0, twinSpan.offsetTop - siblingNode.offsetHeight * .5);
+                  if (direction == "column" && siblingNode.classList.contains("result")) {
+                    siblingNode.scrollTo(0, twinSpan.offsetTop - siblingNode.offsetHeight * .5 - parentNode.offsetHeight);
+                  } else {
+                    siblingNode.scrollTo(0, twinSpan.offsetTop - siblingNode.offsetHeight * .5);
+                  }
                 }
               }
             ]
           }))
         }
       }
+      const container = prompt.createCommandsContainer() as HTMLDivElement
+      // TODO: prefs: direction
+      const directions = ["row", "column"]
+      const direction = directions[1]
+      // @ts-ignore
+      container.style = `
+          display: flex;
+          flex-direction: ${direction};
+          padding: .5em 1em;
+          margin-left: 0px;
+          width: 100%;
+          height: 25em;
+        `
+      const props = {
+        styles: {
+          height: "100%",
+          width: "100%",
+          minWidth: "10em",
+          minHeight: "5em",
+          border: "1px solid #eee",
+          textAlign: "justify",
+          padding: ".5em",
+          fontSize: "1em",
+          lineHeight: "1.5em",
+          overflowY: "auto"
+        },
+      }
       const rawDiv = ztoolkit.UI.createElement(document, "div", {
-        ...props
+        ...props,
+        classList: ["raw"]
       })
       addSentences(rawDiv, rawText, "[\\.;]")
       const resultDiv = ztoolkit.UI.createElement(document, "div", {
         ...props,
+        classList: ["result"]
       })
       addSentences(resultDiv, resultText, "[。;；]")
-      container.append(rawDiv, resultDiv)
+      const size = 5
+      const resizer = ztoolkit.UI.createElement(document, "div", {
+        styles: {
+          height: (direction == "row" ? "100%" : `${size}px`),
+          width: (direction == "column" ? "100%" : `${size}px`),
+          backgroundColor: "#f0f0f0",
+          cursor: direction == "column" ? "ns-resize" : "ew-resize",
+        },
+      })
+      // 可调
+      let y = 0, x = 0;
+      let h = 0, w = 0;
+      const rect = container.getBoundingClientRect();
+      const H = rect.height;
+      const W = rect.width;
+      const mouseDownHandler = function (e: any) {
+        // hide
+        rawDiv.childNodes.forEach((e: any) => e.style.display = "none")
+        resultDiv.childNodes.forEach((e: any) => e.style.display = "none")
+        y = e.clientY;
+        x = e.clientX;
+        const rect = resultDiv.getBoundingClientRect()
+        h = rect.height;
+        w = rect.width;
+        document.addEventListener('mousemove', mouseMoveHandler);
+        document.addEventListener('mouseup', mouseUpHandler);
+      };
+      const mouseMoveHandler = function (e: any) {
+        const dy = e.clientY - y;
+        const dx = e.clientX - x;
+        if (direction == "column") {
+          resultDiv.style.height = `${h - dy}px`;
+          rawDiv.style.height = `${H - (h - dy) - size}px`;
+        }
+        if (direction == "row") {
+          resultDiv.style.width = `${w - dx}px`;
+          rawDiv.style.width = `${W - (w - dx) - size}px`;
+        }
+      };
+      const mouseUpHandler = function () {
+        // show
+        rawDiv.childNodes.forEach((e: any) => e.style.display = "")
+        resultDiv.childNodes.forEach((e: any) => e.style.display = "")
+        document.removeEventListener('mousemove', mouseMoveHandler);
+        document.removeEventListener('mouseup', mouseUpHandler);
+      };
+      resizer.addEventListener('mousedown', mouseDownHandler);
+      // resizer
+      container.append(rawDiv, resizer, resultDiv)
     }
   }])
 }
