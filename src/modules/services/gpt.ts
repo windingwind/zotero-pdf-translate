6da2d5c900c8a0d792dae5b2bd30e0e6566375ae
maasea <351961611@qq.com>
2023-03-19 00:00:27 +0800

    feat: GPT supports stream transfer

--- src/modules/services/gpt.ts
+++ src/modules/services/gpt.ts
@@ -1,23 +1,78 @@
 import { TranslateTaskProcessor } from "../../utils/translate";
+import { getPref } from "../../utils/prefs";
 
-export default <TranslateTaskProcessor>async function (data) {
+export const gptTranslate = <TranslateTaskProcessor>async function (data) {
+  const model = getPref("gptModel");
   const xhr = await Zotero.HTTP.request(
     "POST",
     "https://api.openai.com/v1/chat/completions",
     {
       headers: {
         "Content-Type": "application/json",
-        "Authorization": `Bearer ${data.secret}`,
+        Authorization: `Bearer ${data.secret}`,
       },
       body: JSON.stringify({
-        model: "gpt-3.5-turbo",
-        messages: [{"role": "user", "content": `Translate this passage from ${data.langfrom.split("-")[0]} to ${data.langto.split("-")[0]}: ${data.raw}`}],
+        model: model,
+        messages: [
+          {
+            role: "user",
+            content: `Translate this passage from ${
+              data.langfrom.split("-")[0]
+            } to ${data.langto.split("-")[0]}: ${data.raw}`,
+          },
+        ],
+        stream: true,
       }),
+      requestObserver: (xmlhttp: XMLHttpRequest) => {
+        let preLength = 0;
+        let result = "";
+        xmlhttp.onprogress = (e: any) => {
+          // Only concatenate the new strings
+          let newResponse = e.target.response.slice(preLength);
+          let dataArray = newResponse.split("data: ");
+          for (let data of dataArray) {
+            if (data && !data.includes("DONE")) {
+              let obj = JSON.parse(data);
+              let content = obj.choices[0].delta.content || "";
+              result += content;
+            }
+          }
+          preLength = e.target.response.length;
+          // Removing \n in front of data
+          data.result = result.replace(/^\n\n/, "");
+          addon.hooks.onReaderPopupRefresh();
+          addon.hooks.onReaderTabPanelRefresh();
+        };
+      },
+    }
+  );
+  if (xhr?.status !== 200) {
+    throw `Request error: ${xhr?.status}`;
+  }
+  // data.result = xhr.response.choices[0].message.content.substr(2);
+};
+
+export const updateGPTModel = async function (secret: string) {
+  const xhr = await Zotero.HTTP.request(
+    "GET",
+    "https://api.openai.com/v1/models",
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${secret}`,
+      },
       responseType: "json",
     }
   );
   if (xhr?.status !== 200) {
     throw `Request error: ${xhr?.status}`;
   }
-  data.result = xhr.response.choices[0].message.content.substr(2);
+  const models = xhr.response.data;
+  const availableModels = [];
+  for (const model of models) {
+    if (model.id.includes("gpt")) {
+      availableModels.push(model.id);
+    }
+  }
+  return availableModels;
 };
