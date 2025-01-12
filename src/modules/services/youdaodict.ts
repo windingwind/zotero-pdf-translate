ecb2b12ddd9b546f1a63b6dd6f8ceb0b3abc85e6
howdareyou <feng.xiaoyu.fxy@outlook.com>
2025-01-12 13:59:06 +0800

    feat: add pronunciation api for youdao dictionary service

--- src/modules/services/youdaodict.ts
+++ src/modules/services/youdaodict.ts
@@ -11,18 +11,47 @@ export default <TranslateTaskProcessor>async function (data) {
   }
 
   let res = xhr.response;
-  res = res.replace(/(\r\n|\n|\r)/gm, "");
-  res = res.match(
-    /<div id="phrsListTab.*webTrans" class="trans-wrapper trans-tab">/gm,
-  );
+  try {
+    res = res.replace(/(\r\n|\n|\r)/gm, "");
+    res = res.match(
+      /<div id="phrsListTab.*webTrans" class="trans-wrapper trans-tab">/gm,
+    );
 
-  let tgt = "";
-  if (res.length > 0) {
-    tgt = res[0].replace(/<[^>]*>?/gm, "\n");
-    tgt = tgt.replace(/\n\s*\n/g, "\n");
-    tgt = tgt.replace(/\s\s+/g, " ");
-    tgt = tgt.trim();
+    let tgt = "";
+    if (res.length > 0) {
+      tgt = res[0].replace(/<[^>]*>?/gm, "\n");
+      tgt = tgt.replace(/\n\s*\n/g, "\n");
+      tgt = tgt.replace(/\s\s+/g, " ");
+      tgt = tgt.trim();
+    }
+    if (tgt.length != 0) {
+      const audioList: Array<{ text: string; url: string }> = [];
+
+      const en = tgt.match(
+        /英 \[.+?\]/gm,
+      );
+      if (en != null && en.length != 0) {
+        audioList.push({
+          text: en[0],
+          url: `https://dict.youdao.com/dictvoice?audio=${data.raw}&type=1`
+        })
+      }
+
+      const us = tgt.match(
+        /美 \[.+?\]/gm,
+      );
+      if (us != null && us.length != 0) {
+        audioList.push({
+          text: us[0],
+          url: `https://dict.youdao.com/dictvoice?audio=${data.raw}&type=2`
+        })
+      }
+      data.audio = audioList;
+    }
+
+    data.result = tgt;
+
+  } catch (e) {
+    throw "Parse Error";
   }
-
-  data.result = tgt;
 };
