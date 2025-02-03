ebb1ce8fd04aaf011ed18883465bb49ec640678c
windingwind <33902321+windingwind@users.noreply.github.com>
2025-02-03 09:08:20 +0100

    chore: update deps

--- src/utils/prefs.ts
+++ src/utils/prefs.ts
@@ -1,22 +1,62 @@
 import { config } from "../../package.json";
 
-export function getPref(key: string) {
-  return Zotero.Prefs.get(`${config.prefsPrefix}.${key}`, true);
+export {
+  getPref,
+  setPref,
+  clearPref,
+  getPrefJSON,
+  registerPrefObserver,
+  unregisterPrefObserver,
+};
+
+type _PluginPrefsMap = _ZoteroTypes.Prefs["PluginPrefsMap"];
+
+function getPref<K extends keyof _PluginPrefsMap>(key: K): _PluginPrefsMap[K];
+function getPref(key: string): string | number | boolean;
+function getPref(key: string): string | number | boolean {
+  return Zotero.Prefs.get(`${config.prefsPrefix}.${key}`, true) as any;
 }
 
-export function setPref(key: string, value: string | number | boolean) {
+function setPref<K extends keyof _PluginPrefsMap>(
+  key: K,
+  value: _PluginPrefsMap[K],
+): void;
+function setPref(key: string, value: string | number | boolean): void;
+function setPref(key: string, value: string | number | boolean) {
   return Zotero.Prefs.set(`${config.prefsPrefix}.${key}`, value, true);
 }
 
-export function clearPref(key: string) {
+function clearPref<K extends keyof _PluginPrefsMap>(key: K): void;
+function clearPref(key: string): void;
+function clearPref(key: string) {
   return Zotero.Prefs.clear(`${config.prefsPrefix}.${key}`, true);
 }
 
-export function getPrefJSON(key: string) {
+function getPrefJSON(key: string) {
   try {
     return JSON.parse(String(getPref(key) || "{}"));
   } catch (e) {
     setPref(key, "{}");
   }
   return {};
+}
+
+function registerPrefObserver<K extends keyof _PluginPrefsMap>(
+  key: K,
+  callback: (value: _PluginPrefsMap[K]) => void,
+): symbol;
+function registerPrefObserver(
+  key: string,
+  callback: (value: any) => void,
+): symbol;
+function registerPrefObserver(key: string, callback: (value: any) => void) {
+  return Zotero.Prefs.registerObserver(
+    `${config.prefsPrefix}.${key}`,
+    callback,
+    true,
+  );
+}
+
+function unregisterPrefObserver(observerID: symbol) {
+  return Zotero.Prefs.unregisterObserver(observerID);
 }
