f3995416176687dbda29f934e7f6975783bafff1
windingwind <33902321+windingwind@users.noreply.github.com>
2025-02-04 21:59:41 +0100

    chore: update shortcuts

--- src/modules/shortcuts.ts
+++ src/modules/shortcuts.ts
@@ -2,9 +2,13 @@ export function registerShortcuts() {
   ztoolkit.Keyboard.register((ev, data) => {
     if (data.type === "keyup" && data.keyboard) {
       if (data.keyboard.equals("accel,T")) {
-        const win = Zotero.getMainWindow();
-        if (Services.focus.activeWindow == win) {
-          addon.hooks.onShortcuts(win.Zotero_Tabs.selectedType);
+        const isReaderWindow =
+          ev.target?.ownerGlobal?.location?.href ===
+          "chrome://zotero/content/reader.xhtml";
+        if (!isReaderWindow) {
+          addon.hooks.onShortcuts(
+            Zotero.getMainWindow().Zotero_Tabs.selectedType,
+          );
         } else {
           addon.hooks.onShortcuts("reader");
         }
