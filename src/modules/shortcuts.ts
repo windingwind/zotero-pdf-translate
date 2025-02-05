9e861eaba7b3d06912b6c44bc47600aa92ea9e53
howdareyou <89762586+howdareyou@users.noreply.github.com>
2025-02-05 14:44:45 +0800

    fix: ensure shortcuts work when tab is moved to separate window (#1080)
    * chore(deps-dev): bump zotero-plugin-scaffold in the all-non-major group
    
    Bumps the all-non-major group with 1 update: [zotero-plugin-scaffold](https://github.com/northword/zotero-plugin-scaffold/tree/HEAD/packages/scaffold).
    
    Updates `zotero-plugin-scaffold` from 0.2.1 to 0.2.2
    - [Release notes](https://github.com/northword/zotero-plugin-scaffold/releases)
    - [Commits](https://github.com/northword/zotero-plugin-scaffold/commits/v0.2.2/packages/scaffold)
    
    ---
    updated-dependencies:
    - dependency-name: zotero-plugin-scaffold
      dependency-type: direct:development
      update-type: version-update:semver-patch
      dependency-group: all-non-major
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * fix: ensure shortcuts work when tab is moved to separate window
    
    * chore: update shortcuts
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: windingwind <33902321+windingwind@users.noreply.github.com>

--- src/modules/shortcuts.ts
+++ src/modules/shortcuts.ts
@@ -2,9 +2,16 @@ export function registerShortcuts() {
   ztoolkit.Keyboard.register((ev, data) => {
     if (data.type === "keyup" && data.keyboard) {
       if (data.keyboard.equals("accel,T")) {
-        addon.hooks.onShortcuts(
-          Zotero.getMainWindow().Zotero_Tabs.selectedType,
-        );
+        const isReaderWindow =
+          ev.target?.ownerGlobal?.location?.href ===
+          "chrome://zotero/content/reader.xhtml";
+        if (!isReaderWindow) {
+          addon.hooks.onShortcuts(
+            Zotero.getMainWindow().Zotero_Tabs.selectedType,
+          );
+        } else {
+          addon.hooks.onShortcuts("reader");
+        }
       }
     }
     if (data.type === "keydown") {
